8e8c71f81f36c0adc52f6ce4dbfc67e2531284a3
Edon Budakova <ebudakova99@gmail.com>
2021-12-31 10:59:18 +0100

    update

--- src/App.js
+++ src/App.js
@@ -9,7 +9,14 @@ const App = () => {
             <div className=''>
                 <Header />
                 <Navbar />
-                <Section1 />
+
+                <div className='section-container'>
+                    <Photo style={{order:'1'}} p={''} h1={'How to Dismatle a Nuclear Bomb'} logo={'https://cms.outrider.org/sites/default/files/styles/near_square_md/public/2020-05/Screen%20Shot%202019-03-28%20at%202.42.27%20PM.png?h=8f74817f&amp;itok=vKIgW4LX'} />
+                    <TextSection p={'It is possible to dismantle our nuclear weapons. Over the years, the U.S. and Russia have reduced their arsenals to less than one-sixth of what they were during the Cold War. The current security climate threatens to undo all that work, but we can choose another path. Learn how you can help.'}
+                                h3={'Solutions'}/>
+                 </div>
+
+
                 <Photo logo={'https://cms.outrider.org/sites/default/files/styles/wide_lg/public/2018-02/ARC_CL_BQ_GTY_56455409_master.jpg?h=9a3874b6&amp;itok=6USK-MnG'} 
                 p={'Article'} h1={'What You Can Do About Nuclear Weapons'}
                 style={{ width: '91%' }}
