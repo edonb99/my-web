ddf0ff292a748980803972a1e688b98794b92655
Edon Budakova <ebudakova99@gmail.com>
2022-01-01 12:52:27 +0100

    update

--- src/App.js
+++ src/App.js
@@ -11,24 +11,23 @@ const App = () => {
                 <Navbar />
 
                 <div className='section-container'>
+
                     <Photo style={{order:'0'}} p={''} h1={'How to Dismatle a Nuclear Bomb'} logo={'https://cms.outrider.org/sites/default/files/styles/near_square_md/public/2020-05/Screen%20Shot%202019-03-28%20at%202.42.27%20PM.png?h=8f74817f&amp;itok=vKIgW4LX'} />
                     <TextSection style={{order:'0'}} p={'It is possible to dismantle our nuclear weapons. Over the years, the U.S. and Russia have reduced their arsenals to less than one-sixth of what they were during the Cold War. The current security climate threatens to undo all that work, but we can choose another path. Learn how you can help.'}
                                 h3={'Solutions'}/>
-                 
 
+                    <Photo logo={'https://cms.outrider.org/sites/default/files/styles/wide_lg/public/2018-02/ARC_CL_BQ_GTY_56455409_master.jpg?h=9a3874b6&itok=6USK-MnG'} 
+                    p={'Article'} h1={'What You Can Do About Nuclear Weapons'}
+                    style={{'grid-column':'span 3', order:'1' }}
+                    />
 
-                <Photo logo={'https://cms.outrider.org/sites/default/files/styles/wide_lg/public/2018-02/ARC_CL_BQ_GTY_56455409_master.jpg?h=9a3874b6&itok=6USK-MnG'} 
-                p={'Article'} h1={'What You Can Do About Nuclear Weapons'}
-                style={{'grid-column':'span 3', order:'1' }}
-                />
+                    <TextSection h3={'Impacts'} p={'Nuclear weapons are the most destructive weapons on the planet. You may not even realize how powerful they really are. Use this tool to better understand the effects of a nuclear detonation. Learn more about how the fireball, shock wave, radiation, and heat would separately impact a targeted city.'}
+                        style={{ padding:'80px', 'grid-column':'span 3', order:'1'}}
+                    />
 
-                <TextSection h3={'Impacts'} p={'Nuclear weapons are the most destructive weapons on the planet. You may not even realize how powerful they really are. Use this tool to better understand the effects of a nuclear detonation. Learn more about how the fireball, shock wave, radiation, and heat would separately impact a targeted city.'}
-                    style={{ padding:'80px', 'grid-column':'span 3', order:'1'}}
-                />
-
-                <Photo p={'Interactive'} h1={'What happens in a bomb blast'}  style={{'grid-column':'span 3', order:'2'}} 
-                logo={'https://cms.outrider.org/sites/default/files/styles/wide_lg/public/2018-03/us-nuclear-weapons-test-at-eniwetok-in-1952_7936245684_o.jpg?itok=WkjA590x'} 
-                />
+                    <Photo p={'Interactive'} h1={'What happens in a bomb blast'}  style={{'grid-column':'span 3', order:'2'}} 
+                    logo={'https://cms.outrider.org/sites/default/files/styles/wide_lg/public/2018-03/us-nuclear-weapons-test-at-eniwetok-in-1952_7936245684_o.jpg?itok=WkjA590x'} 
+                    />
 
                     <TextSection p={'A lack of political will is the main barrier to getting rid of nuclear weapons. The technical know-how is there, but countries believe they need their nuclear weapons for security. Learn more about the rivalries that drive countries to pursue nuclear weapons.'}
                                 h3={'Politics'} style={{order:'3'}}/>
@@ -38,9 +37,9 @@ const App = () => {
                     <Photo style={{'grid-column':'span 1', order:'7'}} p={'Article'} h1={'North Korea on the Brink'} logo={'https://cms.outrider.org/sites/default/files/styles/near_square_md/public/2018-02/ARC_CL_BQ_GTY_851817428_master.jpg?h=b1dadf27&itok=ZTk5pIz7'} />
                     <Photo style={{'grid-column':'span 1', order:'8'}} p={'Article'} h1={'Will India and Pakistan’s Conflict Go Nuclear?'} logo={'https://cms.outrider.org/sites/default/files/styles/near_square_md/public/2018-02/ARC_CL_BQ_GTY_498922226_master.jpg?h=cb0d593c&itok=zYqAzruU'} />
 
-                 <TextSection h3={'Costs and Risks'} p={'It doesn’t take a war for nuclear weapons to hurt people. The truth is that nuclear weapons cause ongoing harm, including environmental contamination and exploitation of marginalized communities. Learn more about the costs and risks of just having nuclear weapons, even if they remain unused.'}
-                 style={{ padding:'80px', 'grid-column':'span 3', order:'10'}}
-                    />
+                    <TextSection h3={'Costs and Risks'} p={'It doesn’t take a war for nuclear weapons to hurt people. The truth is that nuclear weapons cause ongoing harm, including environmental contamination and exploitation of marginalized communities. Learn more about the costs and risks of just having nuclear weapons, even if they remain unused.'}
+                    style={{ padding:'80px', 'grid-column':'span 3', order:'10'}}
+                        />
 
                     <Photo style={{'grid-column':'span 2', order:'11'}} p={'Article'} h1={'The Human Cost'} logo={'https://cms.outrider.org/sites/default/files/styles/wide_lg/public/2018-02/ARC_CL_BQ_GTY_118368869_master.jpg?h=0c170278&itok=5ng7-FWT'}/>
                     <Photo style={{'grid-column':'span 1', order:'12'}} p={'Article'} h1={'Smugglers, Thieves, and Terrorists'} logo={'https://cms.outrider.org/sites/default/files/styles/near_square_md/public/2018-02/ARC_CL_BQ_ALA_BH3N28_nuclear_reactor.jpg?h=9a3874b6&itok=epCCpSmV'}/>
@@ -58,17 +57,24 @@ const App = () => {
                     <Photo style={{'grid-column':'span 3', order:'19' }} p={'Timeline'} h1={'Nuclear History: The Current Era'} logo={'https://cms.outrider.org/sites/default/files/styles/wide_lg/public/2018-02/ARC_CL_BQ_GTY_535308894_master.jpg?h=973df0d5&itok=9XApdCTQ'} />
                     <Photo style={{'grid-column':'span 3', order:'20' }} p={'Timeline'} h1={'Hiroshima & Nagasaki: the 76th Anniversary of the Atomic Bombings'} logo={'https://cms.outrider.org/sites/default/files/styles/wide_lg/public/2020-07/GettyImages-78964772.jpg?h=34e32d37&itok=pxClM1Ok'} />
 
-                    <Photo style={{'grid-column':'span 1', order:'21' }} p={'Article'} h1={'Hiroshima & Nagasaki: the 76th Anniversary of the Atomic Bombings'} logo={'https://cms.outrider.org/sites/default/files/styles/wide_lg/public/2020-07/GettyImages-78964772.jpg?h=34e32d37&itok=pxClM1Ok'} />
+                    <Photo style={{'grid-column':'span 1', order:'21' }} p={'Article'} h1={'Sabotaging the Nazi Bomb'} logo={'https://cms.outrider.org/sites/default/files/styles/near_square_md/public/2018-02/ARC_CL_BQ_NIA_sleigh.jpeg?itok=XW5PYxZm'} />
                     <Photo style={{'grid-column':'span 1', order:'22' }} p={'Article'} h1={'Hiroshima & Nagasaki: the 76th Anniversary of the Atomic Bombings'} logo={'https://cms.outrider.org/sites/default/files/styles/wide_lg/public/2020-07/GettyImages-78964772.jpg?h=34e32d37&itok=pxClM1Ok'} />
                     <Photo style={{'grid-column':'span 1', order:'23' }} p={'Article'} h1={'Hiroshima & Nagasaki: the 76th Anniversary of the Atomic Bombings'} logo={'https://cms.outrider.org/sites/default/files/styles/wide_lg/public/2020-07/GettyImages-78964772.jpg?h=34e32d37&itok=pxClM1Ok'} />
 
-                    <Photo style={{'grid-column':'span 1', order:'24' }} p={'Article'} h1={'Hiroshima & Nagasaki: the 76th Anniversary of the Atomic Bombings'} logo={'https://cms.outrider.org/sites/default/files/styles/wide_lg/public/2020-07/GettyImages-78964772.jpg?h=34e32d37&itok=pxClM1Ok'} />
+                    <Photo style={{'grid-column':'span 1', order:'24' }} p={'Article'} h1={'The Nuclear Freeze Movement'} logo={'https://cms.outrider.org/sites/default/files/styles/near_square_md/public/2018-02/ARC_CL_BQ_GTY_56455409_master-slideshow_0.jpg?h=b8aae163&itok=DIKx7nft'} />
                     <Photo style={{'grid-column':'span 1', order:'25' }} p={'Article'} h1={'Hiroshima & Nagasaki: the 76th Anniversary of the Atomic Bombings'} logo={'https://cms.outrider.org/sites/default/files/styles/wide_lg/public/2020-07/GettyImages-78964772.jpg?h=34e32d37&itok=pxClM1Ok'} />
                     <Photo style={{'grid-column':'span 1', order:'26' }} p={'Article'} h1={'Hiroshima & Nagasaki: the 76th Anniversary of the Atomic Bombings'} logo={'https://cms.outrider.org/sites/default/files/styles/wide_lg/public/2020-07/GettyImages-78964772.jpg?h=34e32d37&itok=pxClM1Ok'} />
 
+                    <Photo style={{'grid-column':'span 1', order:'24' }} p={'Article'} h1={'Project Iceworm'} logo={'https://cms.outrider.org/sites/default/files/styles/near_square_md/public/2018-05/GettyImages-179675095.jpg?h=0edc127b&itok=1N0xqRBM'} />
+                    <Photo style={{'grid-column':'span 1', order:'25' }} p={'Article'} h1={'Hiroshima & Nagasaki: the 76th Anniversary of the Atomic Bombings'} logo={'https://cms.outrider.org/sites/default/files/styles/wide_lg/public/2020-07/GettyImages-78964772.jpg?h=34e32d37&itok=pxClM1Ok'} />
+                    <Photo style={{'grid-column':'span 1', order:'26' }} p={'Article'} h1={'Hiroshima & Nagasaki: the 76th Anniversary of the Atomic Bombings'} logo={'https://cms.outrider.org/sites/default/files/styles/wide_lg/public/2020-07/GettyImages-78964772.jpg?h=34e32d37&itok=pxClM1Ok'} />
 
-</div>
-</div>
+                    <Photo style={{'grid-column':'span 1', order:'24' }} p={'Article'} h1={'The Gendered Impacts of the Hiroshima & Nagasaki Bombings'} logo={'https://cms.outrider.org/sites/default/files/styles/near_square_md/public/2020-07/GettyImages-53313850-crop%282%29_0.jpeg?h=5f2cda5c&itok=38Q1vuWt'} />
+                    <Photo style={{'grid-column':'span 1', order:'25' }} p={'Article'} h1={'Hiroshima & Nagasaki: the 76th Anniversary of the Atomic Bombings'} logo={'https://cms.outrider.org/sites/default/files/styles/wide_lg/public/2020-07/GettyImages-78964772.jpg?h=34e32d37&itok=pxClM1Ok'} />
+                    <Photo style={{'grid-column':'span 1', order:'26' }} p={'Article'} h1={'Hiroshima & Nagasaki: the 76th Anniversary of the Atomic Bombings'} logo={'https://cms.outrider.org/sites/default/files/styles/wide_lg/public/2020-07/GettyImages-78964772.jpg?h=34e32d37&itok=pxClM1Ok'} />
+
+                </div>
+             </div>
         </div>
     );
 }
